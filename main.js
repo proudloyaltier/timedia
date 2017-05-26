e6bde580debd91d9b82b7ed49050f746d4a363f2
proudloyaltier (plt) <proudloyaltier@hotmail.com>
2017-05-26 17:18:02 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+function showHome() {
+	document.getElementById('content').innerHTML = home;
+}
+
+function showNews() {
+	document.getElementById('content').innerHTML = news;
+}
+
+function showMoney() {
+	document.getElementById('content').innerHTML = money;
+}
+
+function showGears() {
+	document.getElementById('content').innerHTML = gears;
+}
