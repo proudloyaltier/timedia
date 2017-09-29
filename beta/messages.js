904bf6d0e7a5bd343c9df57110415f378bc89a17
proudloyaltier (plt) <timediamail@gmail.com>
2017-09-29 17:32:20 -0400

    Update messages.js

--- beta/messages.js
+++ beta/messages.js
@@ -19,7 +19,7 @@ if (!localStorage.conversations == undefined || !localStorage.conversations == "
     
   for (var i = 0; i < conversations_split.length; i++) {
     document.getElementById('tabs').innerHTML = document.getElementById('tabs').innerHTML + '<li><a data-toggle="pill" href="#chat' + conversations + '">' + conversations_split[i] + '</a></li>';
-    document.getElementById('msgs').innerHTML = document.getElementById('msgs').innerHTML + '    <div id="chat' + conversations + '" class="tab-pane fade in"><h3>' + conversations_split[i] + '</h3><p></p></div>';
+    document.getElementById('msgs').innerHTML = document.getElementById('msgs').innerHTML + '    <div id="chat' + conversations + '" class="tab-pane fade in"><h3>' + conversations_split[i] + '</h3><input type="text" id="input-chat' + conversations + '" class="form-control" placeholder="Send a message..."></div>';
     
     conversations++;
   }
