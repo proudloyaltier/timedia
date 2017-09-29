ca9d1c992fb5708c3a3d2564835184696f2eca0e
proudloyaltier (plt) <timediamail@gmail.com>
2017-09-29 15:37:39 -0400

    Update messages.js

--- beta/messages.js
+++ beta/messages.js
@@ -1,8 +1,23 @@
+getFromDatabase("conversations");
 var conversations = 3;
 
 function newConversation() {
   var user = prompt("Username");
   
+  if (user == localStorage.name) {
+      alert("You can't make a conversation with yourself.");
+  } else {
+    
+  if (localStorage.includes(user)) {
+      alert("You've already started a conversation with this person.");
+  } else {
+  
   document.getElementById('tabs').innerHTML = document.getElementById('tabs').innerHTML + '<li><a data-toggle="pill" href="#chat' + conversations + '">' + user + '</a></li>';
   document.getElementById('msgs').innerHTML = document.getElementById('msgs').innerHTML + '    <div id="chat' + conversation + '" class="tab-pane fade in active"><h3>' + user + '</h3><p></p></div>';
+
+  if (localStorage.conversations !== undefined) {
+    storeInDatabase("conversations", localStorage.conversations + "," + user);
+  }
+  }
+  }
 }
