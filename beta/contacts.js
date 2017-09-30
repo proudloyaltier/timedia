69ad82813ff04b306f9b16f628c8292aeea6f6af
proudloyaltier (plt) <timediamail@gmail.com>
2017-09-30 16:51:56 -0400

    Update contacts.js

--- beta/contacts.js
+++ beta/contacts.js
@@ -5,3 +5,15 @@ function addContact() {
     localStorage.contacts = localStorage.contacts + ":" + document.getElementById('ticontacts-user').value + "," + document.getElementById('ticontacts-info').value;
   }
 }
+
+function loadContacts() {
+ document.getElementById('ticontacts-contacts').innerHTML = "<ul class="list-group">";
+  
+ for (var i = 0; i < localStorage.contacts.split(",").length; i++) {
+    document.getElementById('ticontacts-contacts').innerHTML = document.getElementById('ticontacts-contacts').innerHTML + '<li class="list-group-item">' + localStorage.contacts.split(",")[i].split(":")[0] +  '<span style="color: gray;">' + localStorage.contacts.split(",")[i].split(":")[1] + '</span></li>';
+ }
+}
+
+if (!localStorage.contacts == undefined) {
+  setInterval(loadContacts, 1000);
+}
