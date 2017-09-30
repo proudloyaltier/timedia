2efcd0d9d1e21f0e0bf137e0f8c582880d8885b9
proudloyaltier (plt) <timediamail@gmail.com>
2017-09-30 18:20:51 -0400

    Update contacts.js

--- beta/contacts.js
+++ beta/contacts.js
@@ -13,7 +13,7 @@ function loadContacts() {
    document.getElementById('ticontacts-contacts').innerHTML = '<ul class="list-group">';
    
    for (var i = 0; i < localStorage.contacts.split(",").length; i++) {
-      document.getElementById('ticontacts-contacts').innerHTML = document.getElementById('ticontacts-contacts').innerHTML + '<li class="list-group-item">' + localStorage.contacts.split(",")[i].split(":")[0] +  ' <span style="color: gray;">' + localStorage.contacts.split(",")[i].split(":")[1] + '</span></li>';
+      document.getElementById('ticontacts-contacts').innerHTML = document.getElementById('ticontacts-contacts').innerHTML + '<li class="list-group-item"><b>' + localStorage.contacts.split(",")[i].split(":")[0] +  '</b> <span style="color: gray;">' + localStorage.contacts.split(",")[i].split(":")[1] + '</span></li>';
    }
  
   document.getElementById('ticontacts-contacts').innerHTML = document.getElementById('ticontacts-contacts').innerHTML + "</ul>";
