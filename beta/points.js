a125bec5cf6a80b083552fc88010c5d7fb3794fa
proudloyaltier (plt) <timediamail@gmail.com>
2017-09-30 17:13:30 -0400

    Update points.js

--- beta/points.js
+++ beta/points.js
@@ -14,8 +14,10 @@ if (localStorage.points !== undefined) {
 function refreshPoints() {
     getFromDatabase("points");
     localStorage.points = Number(localStorage.points);
+    
+    document.getElementById('points-balance').innerHTML = localStorage.points + " TiPoints";
 }
 
 if (getQueryVariable("app") == 1) {
-    setInterval(refreshPoints, 2000);
+    setInterval(refreshPoints, 1000);
 }
