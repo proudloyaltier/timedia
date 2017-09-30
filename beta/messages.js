f8b3711370e0ead1d5bbc296ee4dc0e5ff9dae66
proudloyaltier (plt) <timediamail@gmail.com>
2017-09-30 19:27:13 -0400

    Update messages.js

--- beta/messages.js
+++ beta/messages.js
@@ -1,14 +1,3 @@
-Array.prototype.remove = function() {
-    var what, a = arguments, L = a.length, ax;
-    while (L && this.length) {
-        what = a[--L];
-        while ((ax = this.indexOf(what)) !== -1) {
-            this.splice(ax, 1);
-        }
-    }
-    return this;
-};
-
 var conversations = 0;
 
 if (!localStorage.contacts == undefined) {
