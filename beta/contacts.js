003b9ca03d61977ea6a244cd2e6050f6e3b61e50
proudloyaltier (plt) <timediamail@gmail.com>
2017-09-30 16:59:43 -0400

    Update contacts.js

--- beta/contacts.js
+++ beta/contacts.js
@@ -1,8 +1,8 @@
 function addContact() {
   if (!localStorage.contacts == undefined) {
-    localStorage.contacts = document.getElementById('ticontacts-user').value + "," + document.getElementById('ticontacts-info').value; 
+    localStorage.contacts = document.getElementById('ticontacts-user').value + ":" + document.getElementById('ticontacts-info').value;
   } else {
-    localStorage.contacts = localStorage.contacts + ":" + document.getElementById('ticontacts-user').value + "," + document.getElementById('ticontacts-info').value;
+    localStorage.contacts = localStorage.contacts + "," + document.getElementById('ticontacts-user').value + ":" + document.getElementById('ticontacts-info').value;
   }
 }
 
@@ -10,7 +10,7 @@ function loadContacts() {
  document.getElementById('ticontacts-contacts').innerHTML = '<ul class="list-group">';
   
  for (var i = 0; i < localStorage.contacts.split(",").length; i++) {
-    document.getElementById('ticontacts-contacts').innerHTML = document.getElementById('ticontacts-contacts').innerHTML + '<li class="list-group-item">' + localStorage.contacts.split(",")[i].split(":")[0] +  '<span style="color: gray;">' + localStorage.contacts.split(",")[i].split(":")[1] + '</span></li>';
+    document.getElementById('ticontacts-contacts').innerHTML = document.getElementById('ticontacts-contacts').innerHTML + '<li class="list-group-item">' + localStorage.contacts.split(",")[i].split(":")[0] +  ' <span style="color: gray;">' + localStorage.contacts.split(",")[i].split(":")[1] + '</span></li>';
  }
  
  document.getElementById('ticontacts-contacts').innerHTML = document.getElementById('ticontacts-contacts').innerHTML + "</ul>";
