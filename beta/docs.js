033630ea08d86a5e99478979c3c05857a1e3796b
proudloyaltier (plt) <timediamail@gmail.com>
2017-10-03 13:47:44 -0400

    Update docs.js

--- beta/docs.js
+++ beta/docs.js
@@ -4,8 +4,19 @@ function saveDoc() {
   document.getElementById("url").innerHTML = 'Document URL: ' + url;
 }
 
-if (getQueryVariable("p") !== false) {
+if (getQueryVariable("p") !== false || localStorage.edit !== undefined) {
+  if (localStorage.edit !== undefined) {
+    document.getElementsByClassName('h5').innerHTML = localStorage.edit;
+    localStorage.removeItem('edit');
+  }
+  
   document.getElementById('create').remove();
   document.getElementById('view').style = "visibility: block;";
   document.getElementById('view').innerHTML = atob(getQueryVariable("p"));
+  document.getElementById('tidocs-edit').style = "display: block;";
+}
+
+function editDoc() {
+  localStorage.edit = atob(getQueryVariable("p"));
+  location.reload();
 }
