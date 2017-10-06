934178915dbccf150540328dac693f88662a79a7
proudloyaltier (plt) <timediamail@gmail.com>
2017-10-06 18:17:51 -0400

    Update tiles.js

--- beta/tiles.js
+++ beta/tiles.js
@@ -2,10 +2,10 @@ localStorage.files = Array();
 
 function searchFiles() {
   var search = document.getElementById("TiFilesSearch").value + '';
-  
+
   for (var i = 0; i < localStorage.files.split(",").length; i++) {
     if (localStorage.files.split(",")[i].split(":")[0] == search) {
-      window.open(localStorage.files.split(",")[i].split(":")[1]);
+      location.href = localStorage.files.split(",")[i].split(":")[1];
     }
   }
 }
@@ -14,9 +14,9 @@ function addFile() {
   var title = prompt("Title");
   var upload = prompt("Enter your document URL");
 
-	if (localStorage.files !== undefined) {
-  localStorage.files = localStorage.files + "," + title + ":" + upload;
+  if (localStorage.files !== undefined) {
+    localStorage.files = localStorage.files + "," + title + ":" + upload;
   } else {
-  localStorage.files = title + ":" + upload;
+    localStorage.files = title + ":" + upload;
   }
 }
