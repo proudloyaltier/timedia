e2fe06e0806f404f94cddf782fe280ac0bebdecb
proudloyaltier (plt) <timediamail@gmail.com>
2017-10-06 16:47:01 -0400

    Update settings.js

--- beta/settings.js
+++ beta/settings.js
@@ -1,8 +1,13 @@
 function updateBackgroundImg() {
   localStorage.bgImg = document.getElementById('backgroundImg').value;
+  localStorage.barColor = document.getElementById('barColor').value;
   location.reload();
 }
 
 if (localStorage.bgImg !== undefined) {
   document.getElementById('body').background = localStorage.bgImg;
+}
+
+if (localStorage.barColor !== undefined) {
+  document.getElementById('navbar').style = "background-color: " + localStorage.barColor + ";";
 }
