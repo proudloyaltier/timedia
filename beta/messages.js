54c6046456ddf25d8238a4b79e2a65723ffdde1b
proudloyaltier (plt) <timediamail@gmail.com>
2017-10-06 15:32:00 -0400

    Update messages.js

--- beta/messages.js
+++ beta/messages.js
@@ -4,5 +4,5 @@ if (getQueryVariable("app") == 2) {
 }
 
 function joinChat() {
-  window.open("https://proudloyaltier.github.io/timedia/index.html?app=" + document.getElementById("chat").value);
+  location.replace("https:/proudloyaltier.github.io/timedia/index.html?app=" + document.getElemntById("chat") + "/" )
 }
