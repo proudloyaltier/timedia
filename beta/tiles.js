17570f72a04844dfa7598e284746c52d34234082
proudloyaltier (plt) <timediamail@gmail.com>
2017-10-06 18:14:40 -0400

    Create tiles.js

---
+++ beta/tiles.js
@@ -0,0 +1,22 @@
+localStorage.files = Array();
+
+function searchFiles() {
+  var search = document.getElementById("TiFilesSearch").value + '';
+  
+  for (var i = 0; i < localStorage.files.split(",").length; i++) {
+    if (localStorage.files.split(",")[i].split(":")[0] == search) {
+      window.open(localStorage.files.split(",")[i].split(":")[1]);
+    }
+  }
+}
+
+function addFile() {
+  var title = prompt("Title");
+  var upload = prompt("Enter your document URL");
+
+	if (localStorage.files !== undefined) {
+  localStorage.files = localStorage.files + "," + title + ":" + upload;
+  } else {
+  localStorage.files = title + ":" + upload;
+  }
+}
