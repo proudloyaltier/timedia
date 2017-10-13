aa6f92b517f4256b7b6472010226cd1ada47ce93
proudloyaltier (plt) <timediamail@gmail.com>
2017-10-13 18:23:51 -0400

    Update docs.js

--- beta/docs.js
+++ beta/docs.js
@@ -1,7 +1,7 @@
 
 function saveDoc() {
   var url = window.location.href + '&p=' + btoa(encodeURI(document.getElementsByTagName('h5')[0].innerHTML));
-  
+  localStorage.recentUrl = url;
   window.location.href = url;
 }
 
