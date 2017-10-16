f3c49182efeae48957a24e74f80a01ea2b91017e
proudloyaltier (plt) <timediamail@gmail.com>
2017-10-16 16:15:55 -0400

    Update settings.js

--- beta/settings.js
+++ beta/settings.js
@@ -1,3 +1,14 @@
+function resetTiles() {
+  var resetTiles = confirm("Are you sure you want to delete all files stored in Tiles")
+if (r == true) {
+  localStorage.removeItem('files');
+  storeInDatabase("files", localStorage.files);
+} 
+  else {
+    window.location.reload();
+  }
+}
+
 function updateSettings() {
   localStorage.bgImg = document.getElementById('backgroundImg').value;
   localStorage.barColor = document.getElementById('barColor').value;
