c24d1909d88ad7f20591b52a4e065d775f33724f
proudloyaltier (plt) <timediamail@gmail.com>
2017-10-16 17:27:41 -0400

    Update sheets.js

--- beta/sheets.js
+++ beta/sheets.js
@@ -1,7 +1,11 @@
 function newRow() {
-  document.getElementById('tisheets-table').innerHTML = document.getElementById('tisheets-table').innerHTML + '<tr><td>Name</td><td>Value</td><td>Value</td></tr>';
+  document.getElementById('tisheets-table').innerHTML = document.getElementById('tisheets-table').innerHTML + '<tr id="tisheets-cell"' + document.getElementsByTagName("tr").length + '><td>Name</td><td>Value</td><td>Value</td></tr>';
 }
 
 function newColumn() {
-  
+  for (var i = 0; i < document.getElementsByTagName("tr").length; i++) {
+	    row = document.getElementById("tisheets-cell" + i);
+	    cell = row.insertCell(0);
+	    cell.innerHTML = "Name";
+	}
 }
