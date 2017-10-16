6a19499c6c3f5fbeab08c2a83621c463d6cf1402
proudloyaltier (plt) <timediamail@gmail.com>
2017-10-16 17:12:45 -0400

    Create sheet.js

---
+++ beta/sheet.js
@@ -0,0 +1,7 @@
+function newRow() {
+  document.getElementById('tisheets-table').innerHTML = document.getElementById('tisheets-table').innerHTML + "<tr>    <td></td>    <td></td>    <td></td>  </tr>";
+}
+
+function newColumn() {
+  
+}
