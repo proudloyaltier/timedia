b4f47e59a6f322b8ee1b6f0241250e5a2f1e7f4f
proudloyaltier (plt) <timediamail@gmail.com>
2017-10-19 20:03:55 -0400

    Update tiri.js

--- beta/tiri.js
+++ beta/tiri.js
@@ -2,14 +2,18 @@ if (annyang) {
   // Let's define our first command. First the text we expect, and then the function it should call
   var commands = {
     'open docs': function() {
-      window.location.href = "/timedia/beta?app=3";
+      window.location.href = "?app=3";
     },
     'open sheets': function() {
-      window.location.href = "/timedia/beta?app=4";
+      window.location.href = "?app=4";
     },
     'open tiles': function() {
-      window.location.href = "/timedia/beta?app=7";
+      window.location.href = "?app=7";
+    },
+    'go home': function() {
+      window.location.href = "index.html";
     }
+    'open the chat *password': openChat
   };
 
   // Add our commands to annyang
@@ -17,4 +21,8 @@ if (annyang) {
 
   // Start listening. You can call this here, or attach this call to an event, button, etc.
   annyang.start();
+}
+
+function openChat(password) {
+  window.location.href = "?app=" + password;
 }
