1bc292c0c20035e4ac3315d31cd20ae636e0d15a
proudloyaltier (plt) <timediamail@gmail.com>
2017-10-26 16:53:21 -0400

    Update slides.js

--- beta/slides.js
+++ beta/slides.js
@@ -1,6 +1,7 @@
+var x = 10;
+window.y = 300;
+
 function addText() {
-  var x = 10;
-  window.y = 300;
   var text = prompt("Text");
   var c = document.getElementById("myCanvas");
   var ctx = c.getContext("2d");
