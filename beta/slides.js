8ebc953675df7356e7d7d0a1a15defbfbd75dcca
proudloyaltier (plt) <timediamail@gmail.com>
2017-10-26 16:41:13 -0400

    Create slides.js

---
+++ beta/slides.js
@@ -0,0 +1,7 @@
+function addText() {
+  var text = prompt("Text");
+  var c = document.getElementById("myCanvas");
+  var ctx = c.getContext("2d");
+  ctx.font = "30px Arial";
+  ctx.strokeText(text,0,50);
+}
