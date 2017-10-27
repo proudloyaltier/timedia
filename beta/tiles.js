aefb79f3c88366c30f7b1b8519718179099928da
proudloyaltier (plt) <timediamail@gmail.com>
2017-10-27 14:51:36 -0400

    Update tiles.js

--- beta/tiles.js
+++ beta/tiles.js
@@ -1,4 +1,4 @@
-var tilesInterval = setInterval(refreshTiles, 2500);
+var tilesInterval = setInterval(refreshTiles, 5000);
 
 function resetTiles() {
   var resetTiles = confirm("Are you sure you want to delete all files stored in Tiles?");
