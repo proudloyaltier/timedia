8dd2216426b0b7bbe629328ed03fabfbfebc8fa6
proudloyaltier (plt) <timediamail@gmail.com>
2017-11-04 18:15:48 -0400

    Update tiri.js

--- beta/tiri.js
+++ beta/tiri.js
@@ -11,7 +11,7 @@ if (annyang) {
     //semi-useless easter eggs
     'what is your favorite color': function() {
      responsiveVoice.speak("I do not have eyes but my favorite three number sequence is 26, 159, 162, which is turquoise in javascript");
-      localStorage.ts = '26, 159, 162<br/>Turqouise';
+      localStorage.ts = 'Turqouise';
     },
     'what is your favorite number': function() {
      responsiveVoice.speak("forty two");
@@ -23,66 +23,81 @@ if (annyang) {
     },
     'sing me a song': function() {
      responsiveVoice.speak("la la la la lele leeeeeeeee da, da, da, dididididi lalalalalalalalalllaalla. Sorry. Computers were not meant to sing.");
+      localStorage.ts = 'la la la';
     },
     'tell me a story': function() {
      responsiveVoice.speak("Once upon a time you stopped asking me stupid questions. The End");
+      localStorage.ts = 'Once upon a time you stopped asking stupid questions';
     },
     'hey': function() {
      responsiveVoice.speak("Hey" + "," + localStorage.name);
+      localStorage.ts = 'Hey, ' + localStorage.name;
     },
     'hay': function() {
      responsiveVoice.speak("Hey" + "," + localStorage.name);
+      localStorage.ts = "Hey" + ", " + localStorage.name;
     },
     'hi': function() {
      responsiveVoice.speak("Hi" + "," + localStorage.name);
+      localStorage.ts = "Hi" + ", " + localStorage.name;
     },
     'hello': function() {
      responsiveVoice.speak("Hello" + "," + localStorage.name);
+      localStorage.ts = "Hello" + ", " + localStorage.name;
     },
     'what is up?': function() {
      responsiveVoice.speak("What is up" + "," + localStorage.name);
+      localStorage.ts = "What is up" + ", " + localStorage.name + "?";
     },
     'show me a TI 84 plus': function() {
       window.location.href = "https://upload.wikimedia.org/wikipedia/commons/1/16/TI-84.jpg";
     },
     'do you like to read': function() {
      responsiveVoice.speak("I did, but then I red all of the existing literature in the universe.");
+      localStorage.ts = "I did, but then I red all of the existing literature in the universe.";
     },
     'tell me a knock knock joke': function() {
      responsiveVoice.speak("Knock knock. Interupting virtual assistant, What may I help you with?");
+      localStorage.ts = "Knock knock";
     },
     'tell me a joke': function() {
      responsiveVoice.speak("why did teary cross the road? Because I was trapped in your computer! Ha Ha Ha Ha");
+      localStorage.ts = "why did teary cross the road?<br/>Because I was trapped in your computer!";
     },
-    'are you a democrat or a republican': function() {
-     responsiveVoice.speak("It does not matter. I am not an american citizen, so I can not vote.");
-    },
-    'Kas E O': function() {
+    'kas E O': function() {
      responsiveVoice.speak("Do not say that");
+      localStorage.ts = "Do not say that";
     },
     'T I media sucks': function() {
      responsiveVoice.speak("I can hear you!");
+      localStorage.ts = "I can hear you!";
     },
     'T I media stinks': function() {
      responsiveVoice.speak("I can hear you!");
+      localStorage.ts = "I can hear you!";
     },
     'you are awesome': function() {
      responsiveVoice.speak("Why thank you");
+      localStorage.ts = "Why thank you";
     },
     'you are amazing': function() {
      responsiveVoice.speak("You are too");
+      localStorage.ts = "You are too";
     },
     'you are wonderful': function() {
      responsiveVoice.speak("I know");
+      localStorage.ts = "I know";
     },
     'I have a problem': function() {
      window.location.href = "index.html?app=5";
     },
     'what is your favorite website': function() {
      responsiveVoice.speak("T I media dot T-K");
+      localStorage.ts = "TiMedia";
     },
     'what is your favorite food': function() {
      responsiveVoice.speak("I am a computer software. I can not eat.");
+      localStorage.ts = "I am a computer software. I can not eat.";
     },
     //end of semi-useless easter eggs
     //Commands
