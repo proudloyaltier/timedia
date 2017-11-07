b2743fd6178c24653ad4e331a0c2028a83f464c1
proudloyaltier (plt) <timediamail@gmail.com>
2017-11-07 15:11:44 -0500

    Update tiles.js

--- beta/tiles.js
+++ beta/tiles.js
@@ -123,4 +123,8 @@ if (localStorage.workToSave !== undefined) {
   saveFromTiWork();
 }
 
+if (localStorage.tilesLoaded == true) {
+  document.getElementById("container").style = 'display: none;';
+}
+
 refreshTiles();
