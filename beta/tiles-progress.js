4fed2d7db3218e595e8cc5e4fcc3becd65fcd306
proudloyaltier (plt) <timediamail@gmail.com>
2017-11-07 14:33:33 -0500

    Create tiles-progress.js

---
+++ beta/tiles-progress.js
@@ -0,0 +1,37 @@
+var bar = new ProgressBar.Circle(container, {
+  color: '#aaa',
+  // This has to be the same size as the maximum width to
+  // prevent clipping
+  strokeWidth: 4,
+  trailWidth: 1,
+  easing: 'easeInOut',
+  duration: 5000,
+  text: {
+    autoStyleContainer: false
+  },
+  from: { color: '#aaa', width: 1 },
+  to: { color: '#333', width: 4 },
+  // Set default step function for all animate calls
+  step: function(state, circle) {
+    circle.path.setAttribute('stroke', state.color);
+    circle.path.setAttribute('stroke-width', state.width);
+
+    var value = Math.round(circle.value() * 100);
+    if (value === 0) {
+      circle.setText('');
+    } else {
+      circle.setText(value);
+    }
+
+  }
+});
+bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+bar.text.style.fontSize = '2rem';
+
+bar.animate(1);  // Number from 0.0 to 1.0
+
+setTimeout(reloadTiles, 5000)
+
+function reloadTiles() {
+localStorage.tiles-loaded = true;
+}
