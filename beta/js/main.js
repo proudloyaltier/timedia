d19d7b8170869684659e3bf90098ed17dd48fb31
proudloyaltier (plt) <timediamail@gmail.com>
2017-11-08 18:37:09 -0500

    Update main.js

--- beta/js/main.js
+++ beta/js/main.js
@@ -3,8 +3,3 @@ if (localStorage.access !== undefined) {
   document.getElementById('logout').style = "display: block;";
 }
 
-
-function logOut() {
-  storeInDatabase("points", localStorage.points)
-  window.location.href = "logout.html";
-}
