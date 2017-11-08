b013bc80ae7b1451df5d92c4900a427eda6fd498
proudloyaltier (plt) <timediamail@gmail.com>
2017-11-08 16:15:41 -0500

    Update tipay.js

--- beta/js/tipay.js
+++ beta/js/tipay.js
@@ -1,6 +1,6 @@
 function tiPayButton() {
   var amount = prompt("Amount of TiPoints");
-  var code = '<iframe src="https://proudloyaltier.github.io/timedia/beta/tipaybutton.html?code="' + btoa(amount + "&creator=" + localStorage.name) + '</iframe>';
+  var code = '<iframe src="https://proudloyaltier.github.io/timedia/beta/tipaybutton.html?code=' + btoa(amount + "&creator=" + localStorage.name) + '</iframe>';
   
   prompt("Copy and Paste this Code", code);
 }
