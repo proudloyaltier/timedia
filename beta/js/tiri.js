40ff757660ad3e242874b440893a341119f24770
proudloyaltier (plt) <timediamail@gmail.com>
2017-11-08 18:32:07 -0500

    Update tiri.js

--- beta/js/tiri.js
+++ beta/js/tiri.js
@@ -22,7 +22,7 @@ function tt() {
          localStorage.us = '';
          localStorage.ts = 'welcome';
          
-         localStorage.us = prompt().toLowerCase().replace("?", "").replace("!", "").replace(".", "").replace("what's", "what is");
+         localStorage.us = prompt().toLowerCase().replace("?", "").replace("!", "").replace(".", "").replace("what's", "what is").replace("whats", "what is");
          
          if (localStorage.us == 'i have a problem') {
             window.location.href = "mailto:timediamail@gmail.com";
