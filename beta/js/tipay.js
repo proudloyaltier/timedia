9935aca67faf06879ba1ea51cb43be08b9efeddd
proudloyaltier (plt) <timediamail@gmail.com>
2017-11-08 16:21:36 -0500

    Update tipay.js

--- beta/js/tipay.js
+++ beta/js/tipay.js
@@ -1,6 +1,9 @@
 function tiPayButton() {
   var amount = prompt("Amount of TiPoints");
-  var code = '<iframe src="https://proudloyaltier.github.io/timedia/beta/tipaybutton.html?code=' + btoa(amount + "&creator=" + localStorage.name) + '"</iframe>';
-  
-  prompt("Copy and Paste this Code", code);
+  if (amount < 0) {
+    alert("Negative numbers not allowed.");
+  } else {
+    var code = '<iframe src="https://proudloyaltier.github.io/timedia/beta/tipaybutton.html?code=' + btoa(amount + "&creator=" + localStorage.name) + '"</iframe>';
+    prompt("Copy and Paste this Code", code);
+  }
 }
