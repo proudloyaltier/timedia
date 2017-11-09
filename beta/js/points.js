dec476fe326cdc1e0f9fddd8ca5e66a21019b760
proudloyaltier (plt) <timediamail@gmail.com>
2017-11-09 06:44:27 -0500

    Update points.js

--- beta/js/points.js
+++ beta/js/points.js
@@ -21,3 +21,7 @@ function refreshPoints() {
 if (getQueryVariable("app") == 1) {
     setInterval(refreshPoints, 1000);
 }
+
+function save() {
+    storeInDatabase("points", localStorage.points)
+}
