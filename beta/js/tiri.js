d363e6fdbaa2f3141aeba5bafd96c2b1eaeafce9
proudloyaltier (plt) <timediamail@gmail.com>
2017-11-09 15:47:31 -0500

    Update tiri.js

--- beta/js/tiri.js
+++ beta/js/tiri.js
@@ -32,7 +32,7 @@ function tt() {
             localStorage.ts = 'TI!';
          } else if(localStorage.us.split(" ")[0] == 'solve') {
             responsiveVoice.speak(localStorage.us.split(" ")[1] + " equals " + math.eval(localStorage.us.split(" ")[1]));
-            localStorage.ts = math.eval(localStorage.us.split(" ")[1]);           
+            localStorage.ts = math.eval(localStorage.us.split(" ")[1]).toLocaleString();           
          } else if(localStorage.us.split(" ")[0] + " " + localStorage.us.split(" ")[1] + " " + localStorage.us.split(" ")[2] + " " + localStorage.us.split(" ")[3] == "set a timer for") {
             if (isNaN(Number(localStorage.us.split(" ")[4]))) {
                responsiveVoice.speak("Sorry. I do not understand");
@@ -508,7 +508,7 @@ function goTo(url) {
 }
 
 function solve(problem) {
-  var result = math.eval(problem).toString();
+  var result = math.eval(problem).toLocaleString();
   responsiveVoice.speak(result);
   localStorage.ts = result;
 }
