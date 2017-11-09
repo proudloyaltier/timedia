3dad8f93f549095bab64d650f19222094786cd4f
proudloyaltier (plt) <timediamail@gmail.com>
2017-11-08 21:22:41 -0500

    Update tiri.js

--- beta/js/tiri.js
+++ beta/js/tiri.js
@@ -144,19 +144,12 @@ function tt() {
          } else if(localStorage.us == 'what is your favorite food') {
             responsiveVoice.speak("I am a computer software. I can not eat.");
             localStorage.ts = "I am a computer software. I can not eat."
-         } else if(localStorage.us == 'what time is it') {
+         } else if(localStorage.us == 'what is the time' || localStorage.us == 'what time is it') {
             var da = new Date();
             var na = da.getMinutes();
             var db = new Date();
             var nb = db.getHours();
-            responsiveVoice.speak("it is" + nb + na);
-            localStorage.ts = nb + ":" + na;
-         } else if(localStorage.us == 'what is the time') {
-            var da = new Date();
-            var na = da.getMinutes();
-            var db = new Date();
-            var nb = db.getHours();
-            responsiveVoice.speak("it is" + nb + na);
+            responsiveVoice.speak("It is" + nb + " " + na);
             localStorage.ts = nb + ":" + na;
          } else if(localStorage.us == 'how are you') {
             responsiveVoice.speak("I am good");
