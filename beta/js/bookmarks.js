47eafa6a776ebcadbd7b70c4b732273a9d1ef9ba
proudloyaltier (plt) <timediamail@gmail.com>
2017-11-18 08:51:01 -0500

    Create bookmarks.js

---
+++ beta/js/bookmarks.js
@@ -0,0 +1,25 @@
+
+function saveDoc() {
+  var url = "index.html?app=10"+ '&l=' + btoa(encodeURI(document.getElementsByTagName('h4')[0].innerHTML));
+  localStorage.recentUrl = url;
+  localStorage.workToSave = url;
+  window.location.href = "?app=7";
+}
+
+if (getQueryVariable("l") !== false || localStorage.editBookmark !== undefined) {
+  if (localStorage.edit !== undefined) {
+    document.getElementsByTagName('h4')[0].innerHTML = decodeURI(localStorage.edit);
+    localStorage.removeItem('editBookmark');
+    throw new Error("Opened edit.");
+  }
+  
+  document.getElementById('createBookmark').remove();
+  document.getElementById('viewBookmark').style = "visibility: block;";
+  document.getElementById('viewBookmark').innerHTML = decodeURI(atob(getQueryVariable("l")));
+  document.getElementById('tibookmarks-edit').style = "visibility: block;";
+}
+
+function editBookmark() {
+  localStorage.edit = atob(getQueryVariable("l"));
+  window.location.href = "index.html?app=3";
+}
