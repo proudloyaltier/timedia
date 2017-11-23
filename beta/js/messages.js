2593d594acec4f3a213b4f9ee2d0612db9d96fed
proudloyaltier (plt) <timediamail@gmail.com>
2017-11-23 17:54:21 -0500

    Update messages.js

--- beta/js/messages.js
+++ beta/js/messages.js
@@ -28,14 +28,12 @@ function hideChatPassword() {
   window.location.reload();
 }
 
-if (localStorage.showChatPassword == false) {
-   document.getElementById('show-chat-password').style = "display: block;";
-   document.getElementById('hide-chat-password').style = "display: none;";
-}
-
 if (localStorage.showChatPassword == true) {
   document.getElementById('show-chat-password').style = "display: none;";
   document.getElementById('hide-chat-password').style = "display: block;";
+} else {
+  document.getElementById('show-chat-password').style = "display: block;";
+  document.getElementById('hide-chat-password').style = "display: none;";
 }
 
 if (window.location !== "index.html" && document.title == "Home - TiMedia" && getQueryVariable("app") !== false && localStorage.showChatPassword !== false) {
