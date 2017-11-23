641315dbe973323f803d9d507681aafc9acfad78
proudloyaltier (plt) <timediamail@gmail.com>
2017-11-22 19:40:54 -0500

    Create tiles-button.js

---
+++ beta/js/tiles-button.js
@@ -0,0 +1,27 @@
+var links = [
+{
+  "bgcolor":"#03A9F4",
+  "icon":"+"
+},
+{
+  "url":"https://proudloyaltier.github.io/timedia/beta/index.html?app=3",
+  "bgcolor":"#DB4A39",
+  "color":"#fffff",
+  "icon":"<span class="glyphicon glyphicon-pencil"></span>",
+  "target":"_blank"
+},
+{
+  "url":"https://proudloyaltier.github.io/timedia/beta/index.html?app=4",
+  "bgcolor":"#00ACEE",
+  "color":"#fffff",
+  "icon":"<span class="glyphicon glyphicon-th-list"></span>",
+  "target":"_blank"
+},
+{
+  "url":"https://proudloyaltier.github.io/timedia/beta/index.html?app=10",
+  "bgcolor":"#3B5998",
+  "color":"#fffff",
+  "icon":"<span class="glyphicon glyphicon-bookmark"></span>",
+  "target":"_blank"
+},
+]
