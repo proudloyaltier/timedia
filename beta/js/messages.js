e1149cb54808f0846a2f42276b2da416c78cc5db
proudloyaltier (plt) <timediamail@gmail.com>
2017-11-23 10:26:20 -0500

    Update messages.js

--- beta/js/messages.js
+++ beta/js/messages.js
@@ -1,6 +1,8 @@
 if (getQueryVariable("app") == 2) {
   document.getElementById("chat").style = "";
   document.getElementById("joinChatBtn").style = "";
+  document.getElementById("random-password-notice").style = "";
+  document.getElementById("random-password-btn").style = "";
 }
 
 function joinChat() {
@@ -8,3 +10,11 @@ function joinChat() {
   localStorage.workToSave = url;
   window.location.href = "index.html?app=7";
 }
+
+function randomTiChatPassword() {
+  var password = Math.floor(100000000 + Math.random() * 900000000);
+  var url = "index.html?app=" + password;
+  localStorage.workToSave = url;
+  window.location.href = "index.html?app=7";
+}
+
