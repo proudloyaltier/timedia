c579a097a33bded96f74abb53b77e6367e8d04cd
proudloyaltier (plt) <timediamail@gmail.com>
2017-11-24 20:34:22 -0500

    Create tismile.js

---
+++ beta/js/tismile.js
@@ -0,0 +1,10 @@
+var video = document.getElementById('tismile-video');
+
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({
+    video: true
+  }).then(function(stream) {
+    video.src = window.URL.createObjectURL(stream);
+    video.play();
+  });
+}
