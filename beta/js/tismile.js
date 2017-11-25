573c930c32372b730a7f923d26e221283318b9bb
proudloyaltier (plt) <timediamail@gmail.com>
2017-11-24 20:39:35 -0500

    Update tismile.js

--- beta/js/tismile.js
+++ beta/js/tismile.js
@@ -8,3 +8,17 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     video.play();
   });
 }
+
+function setupTiSmile(stage) {
+   if (stage == 0) {
+     document.getElementById('tismile-video').style = "border-radius: 30px; background-color: black;";
+     document.getElementById('tismile-stage1').style = "";
+   } elseif (stage == 1) {
+     document.getElementById('tismile-video').style = "display: none;";
+     document.getElementById('tismile-stage1').style = "display: none;";
+     document.getElementById('tismile-stage2').style = "";
+   } elseif (stage == 3) {
+     document.getElementById('tismile-stage2').style = "display: none;";
+     document.getElementById('tismile-stage3').style = "";
+   }
+}
