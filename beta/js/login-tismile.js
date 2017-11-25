878ecc34e18e1ade6964c4f5c738d800ce43c0ed
proudloyaltier (plt) <timediamail@gmail.com>
2017-11-25 10:42:32 -0500

    Update login-tismile.js

--- beta/js/login-tismile.js
+++ beta/js/login-tismile.js
@@ -1,3 +1,14 @@
+var video = document.getElementById('tismile-video');
+
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({
+    video: true
+  }).then(function(stream) {
+    video.src = window.URL.createObjectURL(stream);
+    video.play();
+  });
+}
+
 if (localStorage.faces !== undefined) {
   document.getElementById('login-user').style = "display: none;";
   document.getElementById('login-tismile').style = "";
