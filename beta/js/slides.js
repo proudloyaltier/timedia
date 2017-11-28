f1c98adb4273810fd62ed77ab59cbb907a2ae424
proudloyaltier (plt) <timediamail@gmail.com>
2017-11-28 17:50:40 -0500

    Update slides.js

--- beta/js/slides.js
+++ beta/js/slides.js
@@ -1,11 +1,26 @@
+var c = document.getElementById("tislides-canvas");
+var ctx = c.getContext("2d");
+
 var x = 10;
 window.y = 300;
 
 function addText() {
   var text = prompt("Text");
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
   ctx.font = "30px Arial";
   ctx.fillText(text, x, y);
   window.y = y + 50;
+}
+
+ctx.getImageData(0, 0, 1000, 600);
+
+function saveSlide() {
+  var url = "index.html?app=9"+ "&s=" + btoa(JSON.stringify(ctx.getImageData(0, 0, 1000, 600)));
+  localStorage.recentUrl = url;
+  localStorage.workToSave = url;
+  window.location.href = "?app=7";
+}
+
+if (getQueryVariable("s") !== false) {
+  ctx.putImageData(JSON.parse(getQueryVariable("s")));
+  document.getElementById("tislides-save").remove();
 }
