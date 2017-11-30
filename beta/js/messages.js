79293b758e4b5a67f494220666904b1a7052cc50
proudloyaltier (plt) <timediamail@gmail.com>
2017-11-30 13:55:20 -0500

    Update messages.js

--- beta/js/messages.js
+++ beta/js/messages.js
@@ -1,21 +1,15 @@
 if (getQueryVariable("app") == 2) {
   document.getElementById("chat").style = "";
   document.getElementById("joinChatBtn").style = "";
-  document.getElementById("random-password-notice").style = "";
-  document.getElementById("random-password-btn").style = "";
+
 }
 
 function joinChat() {
-  var url = "index.html?app=" + document.getElementById("chat").value;
-  localStorage.workToSave = url;
-  window.location.href = "index.html?app=7";
-}
+var chat = [document.getElementById('chat').value, localStorage.name];
+chat.sort();
+var chatPassword = MD5(chat);
+window.location.href = "index.html?app=" + chatPassword;
 
-function randomTiChatPassword() {
-  var password = Math.floor(100000000 + Math.random() * 900000000);
-  var url = "index.html?app=" + password;
-  localStorage.workToSave = url;
-  window.location.href = "index.html?app=7";
 }
 
 function showChatPassword() {
@@ -25,8 +19,6 @@ function showChatPassword() {
 }
 
 if (window.location !== "index.html" && document.title == "Home - TiMedia" && getQueryVariable("app") !== false) {
-  document.getElementById('chat-password-label').style = "display: block;";
   setInterval(changeName, 50);
 } else {
-  document.getElementById('chat-password-label').style = "display: none;";
 }
