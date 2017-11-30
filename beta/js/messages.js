b5609ef960190b36de4b29c895826937b41801d7
proudloyaltier (plt) <timediamail@gmail.com>
2017-11-30 14:51:06 -0500

    Update messages.js

--- beta/js/messages.js
+++ beta/js/messages.js
@@ -11,8 +11,8 @@ var sortAlphabets = function(text) {
 
 function joinChat() {
 var chat = document.getElementById('chat').value + localStorage.name;
-var chatPassword1 = MD5(chat);
-var chatPassword = sortAlphabets(chatPassword1);
+var chatPassword1 = sortAlphabets(chatPassword1);
+var chatPassword = MD5(chatPassword1);
 window.location.href = "index.html?app=" + chatPassword;
 
 }
