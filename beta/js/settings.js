86f06f39100087b63bde3ae2f5170fc33aa2deb1
proudloyaltier (plt) <timediamail@gmail.com>
2017-12-05 14:08:34 -0500

    Update settings.js

--- beta/js/settings.js
+++ beta/js/settings.js
@@ -25,16 +25,25 @@ function sstismile() {
   document.getElementById("tismileSettings").style = "display: block"; 
   document.getElementById("personalizationSettings").style = "display: none"; 
   document.getElementById("tileSettings").style = "display: none"; 
+  document.getElementById("sspersonal-icon").className = "glyphicon glyphicon-menu-down";
+  document.getElementById("sstiles-icon").className = "glyphicon glyphicon-menu-down";
+  document.getElementById("sstismile-icon").className = "glyphicon glyphicon-menu-up";
 } 
 
 function sstiles() { 
   document.getElementById("tismileSettings").style = "display: none"; 
   document.getElementById("personalizationSettings").style = "display: none"; 
   document.getElementById("tileSettings").style = "display: block"; 
+  document.getElementById("sspersonal-icon").className = "glyphicon glyphicon-menu-down";
+  document.getElementById("sstiles-icon").className = "glyphicon glyphicon-menu-up";
+  document.getElementById("sstismile-icon").className = "glyphicon glyphicon-menu-down";
 } 
 
 function sspersonal() {
   document.getElementById("tismileSettings").style = "display: none"; 
   document.getElementById("personalizationSettings").style = "display: block"; 
   document.getElementById("tileSettings").style = "display: none"; 
+  document.getElementById("sspersonal-icon").className = "glyphicon glyphicon-menu-up";
+  document.getElementById("sstiles-icon").className = "glyphicon glyphicon-menu-down";
+  document.getElementById("sstismile-icon").className = "glyphicon glyphicon-menu-down";
 }
