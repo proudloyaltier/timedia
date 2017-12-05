61ed06332a8f9879b08856207c6b277f516c81ac
proudloyaltier (plt) <timediamail@gmail.com>
2017-12-05 12:34:31 -0500

    Update settings.js

--- beta/js/settings.js
+++ beta/js/settings.js
@@ -1,17 +1,36 @@
-function updateSettings() {
-  localStorage.bgImg = document.getElementById('backgroundImg').value;
-  localStorage.barColor = document.getElementById('barColor').value;
-  location.reload();
-}
 
-if (localStorage.bgImg !== undefined) {
-  document.getElementById('body').background = localStorage.bgImg;
-}
+function updateSettings() { 
+localStorage.bgImg = document.getElementById('backgroundImg').value; 
+localStorage.barColor = document.getElementById('barColor').value; 
+location.reload(); 
+} 
 
-if (localStorage.barColor !== undefined) {
-  document.getElementById('navbar').style = "background-color: #" + localStorage.barColor + ";";
-}
+if (localStorage.bgImg !== undefined) { 
+document.getElementById('body').background = localStorage.bgImg; 
+} 
 
-function lastTile() {
-  window.location.href = localStorage.recentUrl;
+if (localStorage.barColor !== undefined) { 
+document.getElementById('navbar').style = "background-color: #" + localStorage.barColor + ";"; 
+} 
+
+function lastTile() { 
+window.location.href = localStorage.recentUrl; 
+} 
+
+function sstismile() { 
+document.getElementById("tismileSettings").style = "display: block"; 
+document.getElementById("personalizationSettings").style = "display: none"; 
+document.getElementById("tileSettings").style = "display: none"; 
+} 
+
+function sstiles() { 
+document.getElementById("tismileSettings").style = "display: none"; 
+document.getElementById("personalizationSettings").style = "display: none"; 
+document.getElementById("tileSettings").style = "display: block"; 
+} 
+
+function sspersonal() { 
+document.getElementById("tismileSettings").style = "display: none"; 
+document.getElementById("personalizationSettings").style = "display: block"; 
+document.getElementById("tileSettings").style = "display: none"; 
 }
