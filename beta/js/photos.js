d42d0eea091ad227654b5a87ef10b7ae9eb97222
proudloyaltier (plt) <timediamail@gmail.com>
2017-12-08 16:25:38 -0500

    Create photos.js

---
+++ beta/js/photos.js
@@ -0,0 +1,14 @@
+function refreshPhotos() {
+  getFromDatabase("photos");
+}
+
+function loadPhotos() {
+  for (var i = 0; i < localStorage.photos.length) {
+    document.getElementById("photos").innerHTML = document.getElementById("photos").innerHTML + '<img width="500px" src="' + localStorage.photos.split("!!&&")[i] + '">'
+  }
+}
+
+if (getQueryVariable("app") == 12) {
+  setInterval(refreshPhotos, 1000);
+  setInterval(loadPhotos, 1000);
+}
