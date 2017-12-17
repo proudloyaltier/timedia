9bb2d9ffc3afd47738c96cfcd7e0610afb70688d
proudloyaltier (plt) <timediamail@gmail.com>
2017-12-17 16:24:44 -0500

    Create main.js

---
+++ beta/tiritouch/main.js
@@ -0,0 +1,72 @@
+var commands = ["Hello", "Hey", "Set a timer for 5 minutes", "Set a timer for 2 minutes", "Give me a nickname", "What is your name?"];
+var suggested = [];
+var possible = [];
+var length;
+var element = document.getElementById("suggested");
+
+function predict(letters) {
+  suggested = [];
+  if (possible.length == 0) {
+    for (i = 0; i < letters.length; i++) {
+      possible[i] = letters.charAt(i);
+    }
+  } else {
+    var length = possible.length;
+    for (i = 0; i < length; i++) {
+      for (j = 0; j < letters.length; j++) {
+        possible.push(possible[i] + letters.charAt(j));
+      }
+    }
+  }
+
+  for (var k = 0; possible[0].length !== possible[possible.length - 1].length; k++) {
+    possible.splice(possible[k], 1);
+  }
+
+  for (var l = 0; l < commands.length; l++) {
+    for (var m = 0; m < possible.length; m++) {
+      if (commands[l].toLowerCase().includes(possible[m].toLowerCase())) {
+        suggested.push(commands[l]);
+      }
+    }
+  }
+
+  if (!suggested[0]) {
+    element.innerHTML = "";
+  } else {
+    element.innerHTML = suggested[0];
+  }
+}
+
+function typeNum(el) {
+  switch (Number(el.value)) {
+    case 7:
+      predict(" ");
+      break;
+    case 8:
+      predict("ABC");
+      break;
+    case 9:
+      predict("DEF");
+      break;
+    case 4:
+      predict("GHI");
+      break;
+    case 5:
+      predict("JKL");
+      break;
+    case 6:
+      predict("MNO");
+      break;
+    case 1:
+      predict("PQRS");
+      break;
+    case 2:
+      predict("TUV");
+      break;
+    case 3:
+      predict("WXYZ");
+  }
+
+  el.value = "";
+}
