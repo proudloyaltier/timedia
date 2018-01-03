16ddd466b9dfd51c492903db489714cd7da053a4
proudloyaltier (plt) <timediamail@gmail.com>
2018-01-03 13:58:24 -0500

    Delete login-pin.js

--- beta/js/login-pin.js
+++
@@ -1,8 +0,0 @@
-function loginWithPin() {
-  var splitpin = localStorage.pin.split(":");
-  window.pinToCompare = MD5(document.getElementById('pin').value) + "jdksfhjsdhhfjsdfhjhhfhfjehcjenfjdmdncndnvbdmfjskfhenrhshgmnnmnsnfndndnfndnfnengnsnfnshfndnfjsjvjndjd";
-  if (window.pinToCompare == splitpin[0]) {
-  localStorage.name = splitpin[1]
-  localStorage.access = btoa(splitpin[1])
-  window.location.href = "index.html";
-  }
