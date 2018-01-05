b0cc2e2a5717de39d10d6dfef59fc7cbcadb4a67
proudloyaltier (plt) <timediamail@gmail.com>
2018-01-05 16:32:06 -0500

    Update home.js

--- beta/js/home.js
+++ beta/js/home.js
@@ -93,5 +93,5 @@ function customImageUpload() {
      $(".hcb-comment-tb").removeClass('hcb-comment-tb');
        $('.inputfile').removeClass('inputfile');
        $( "label[for='hcb_file']" ).text( "Insert Image" ).addClass('btn').addClass('btn-primary');
-       document.getElementsByClassName('hcb-icon').style = "display: none;";
+       setInterval(function() {document.getElementsByClassName('hcb-icon').style = "display: none;";}, 0);
 }
