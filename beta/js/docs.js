62dd9eb601a643e5e0a8072e6692383df40da918
phyllocrania <34107739+phyllocrania@users.noreply.github.com>
2018-02-08 17:33:26 -0500

    Update docs.js

--- beta/js/docs.js
+++ beta/js/docs.js
@@ -27,7 +27,7 @@ if (getQueryVariable("p") !== false || localStorage.edit !== undefined) {
   document.getElementById('view').style = "visibility: block;";
   document.getElementById('tidocs-edit').style = "visibility: block;";
   document.getElementById('tidocs-reader').style = "visibility: block;";
-  for (window.dbRef !== undefined) {
+  while (window.dbRef !== undefined) {
   var urlRef = window.dbRef.child(getQueryVariable("p"));
   urlRef.on("value", function(snapshot) {
   snapshot.forEach(function(child) {
