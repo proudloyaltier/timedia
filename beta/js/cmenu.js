56ea7fe7904d11fd07bd68b46257fcf604adbc74
proudloyaltier (plt) <timediamail@gmail.com>
2018-03-05 17:45:02 -0500

    Update cmenu.js

--- beta/js/cmenu.js
+++ beta/js/cmenu.js
@@ -1,17 +1,17 @@
-var normalMenu = document.getElementById("cmenu")innerHTML;
+var normalMenu = document.getElementById("context-menu")innerHTML;
 
 function openCmenu(e) {
   e = e || window.event;
   e.preventDefault();
   if(!overTile) {
-    document.getElementById("cmenu").innerHTML = normalMenu;
+    document.getElementById("context-menu").innerHTML = normalMenu;
   }
-  document.getElementById("cmenu").style = "display: inline-block";
-  document.getElementById("cmenu").style.top = ( e.clientY-10) + "px";
-  document.getElementById("cmenu").style.left = e.clientX + "px";
+  document.getElementById("context-menu").style = "display: inline-block";
+  document.getElementById("context-menu").style.top = ( e.clientY-10) + "px";
+  document.getElementById("context-menu").style.left = e.clientX + "px";
 }
 function closeCmenu() {
-  document.getElementById("cmenu").style = "display: none";
+  document.getElementById("context-menu").style = "display: none";
 }
 window.addEventListener("contextmenu", openCmenu);
 window.addEventListener("click", closeCmenu);
