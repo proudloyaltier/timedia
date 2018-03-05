8e16225d9221bc93294e7edbff61d4a72c65c5b2
proudloyaltier (plt) <timediamail@gmail.com>
2018-03-05 17:43:07 -0500

    Update cmenu.js

--- beta/js/cmenu.js
+++ beta/js/cmenu.js
@@ -1,6 +1,11 @@
+var normalMenu = document.getElementById("cmenu")innerHTML;
+
 function openCmenu(e) {
   e = e || window.event;
   e.preventDefault();
+  if(!overTile) {
+    document.getElementById("cmenu").innerHTML = normalMenu;
+  }
   document.getElementById("cmenu").style = "display: inline-block";
   document.getElementById("cmenu").style.top = ( e.clientY-10) + "px";
   document.getElementById("cmenu").style.left = e.clientX + "px";
