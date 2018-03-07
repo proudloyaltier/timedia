70aa4feb1d9b919fcb1ab1e72bc181f9e825c28f
proudloyaltier (plt) <timediamail@gmail.com>
2018-03-07 18:06:24 -0500

    Update tiles.js

--- beta/js/tiles.js
+++ beta/js/tiles.js
@@ -10,9 +10,10 @@ function renameTile(tid, rwith) {
 
 function renameTilePrompt(tid) {
   var replacename = prompt("rename", localStorage.files.split(",")[tid].split("!!")[0]) + "!!" + localStorage.files.split(",")[tid].split("!!")[1]
+  
   if (replacename != null) {
-  localStorage.files = localStorage.files.replace(localStorage.files.split(",")[tid], replacename); 
-  save();
+    localStorage.files = localStorage.files.replace(localStorage.files.split(",")[tid], replacename); 
+    save();
   }
 }
 
