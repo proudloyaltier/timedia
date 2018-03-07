ea112926779333b2ee6fa40acc8fcf93a01be568
proudloyaltier (plt) <timediamail@gmail.com>
2018-03-07 07:48:44 -0500

    Update sheets.js

--- beta/js/sheets.js
+++ beta/js/sheets.js
@@ -51,7 +51,7 @@ if (getQueryVariable("t") !== false || localStorage.editSheet !== undefined) {
 		document.getElementById('tisheets-table').setAttribute("contenteditable", false);
 		document.getElementById('tisheets-save').style = "display: none;";
 		document.getElementById('tisheets-add-row').style = "display: none;";
-		document.getElementById('tisheets-edit').style = "";
+		document.getElementById('tisheets-edit').style.display = ''
 		document.getElementById('tisheets-reader').style = "";
 	}
 }
