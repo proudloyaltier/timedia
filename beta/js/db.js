41f997d42b5466315de58b07a8fa9087905cb8ef
proudloyaltier (plt) <timediamail@gmail.com>
2018-03-22 14:08:47 -0400

    Update db.js

--- beta/js/db.js
+++ beta/js/db.js
@@ -238,6 +238,9 @@ function errorLoading(err) {
 }
 
 function login() {	
+   window.onerror = function(error) {
+      alert('error');
+   }
    var username = document.getElementById('login-username').value;		
    var password = document.getElementById('login-password').value;		
    var hash = MD5(password);		
