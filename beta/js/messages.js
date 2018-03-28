e7079f175dfde72310cd40530765267ecefdb6cb
proudloyaltier (plt) <timediamail@gmail.com>
2018-03-27 21:43:09 -0400

    Update messages.js

--- beta/js/messages.js
+++ beta/js/messages.js
@@ -1,24 +1,16 @@
-if (getQueryVariable("app") == 2) {
-  document.getElementById("chat").style = "";
-  document.getElementById("joinChatBtn").style = "";
-
-}
-
 var sortAlphabets = function(text) {
-    return text.split('').sort().join('');
+  return text.split('').sort().join('');
 };
 
-
 function joinChat() {
-var chat = document.getElementById('chat').value + localStorage.name;
-var chat = chat.toLowerCase();
-var chatPassword1 = sortAlphabets(chat);
-var chatPassword = MD5(chatPassword1);
-window.location.href = "index.html?app=" + chatPassword;
+  var chat = document.getElementById('chat').value + localStorage.name;
+  var chat = chat.toLowerCase();
+  var chatPassword1 = sortAlphabets(chat);
+  var chatPassword = MD5(chatPassword1);
+  window.location.href = "index.html?app=" + chatPassword;
 }
 
 if (window.location !== "index.html" && getQueryVariable("app") !== false && getQueryVariable("app").length > 7) {
   setInterval(changeName, 50);
   document.title = "TiChat - TiMedia"
-} else {
 }
