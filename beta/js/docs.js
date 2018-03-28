bc34464e8c040b512d63b0f4ac0076c8e9a8429a
proudloyaltier (plt) <timediamail@gmail.com>
2018-03-28 18:14:54 -0400

    Update docs.js

--- beta/js/docs.js
+++ beta/js/docs.js
@@ -68,8 +68,8 @@ if (getQueryVariable("p") !== false || localStorage.edit !== undefined) {
 
 function editDoc() {
   localStorage.edit = window.edit;
-  window.location.href = "index.html?app=3";
   localStorage.tidocssave = getQueryVariable("p");
+  window.location.href = "index.html?app=3";
 }
 
 function docReader() {
