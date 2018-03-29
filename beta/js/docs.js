25c36421e1e1c774a6c85249f833a963ddd1bf96
proudloyaltier (plt) <timediamail@gmail.com>
2018-03-29 10:23:51 -0400

    Update docs.js

--- beta/js/docs.js
+++ beta/js/docs.js
@@ -69,6 +69,7 @@ if (getQueryVariable("p") !== false || localStorage.edit !== undefined) {
 }
 
 function editDoc() {
+ localStorage.removeItem('tidocssave')
  localStorage.setItem('tidocssave', getQueryVariable("p"));
  localStorage.edit = window.edit;
  localStorage.editAutoSave = localStorage.edit;
