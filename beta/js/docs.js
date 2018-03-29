dea3d58b9ea8e69d205adbcb80b099604e6fc21a
proudloyaltier (plt) <timediamail@gmail.com>
2018-03-29 05:47:42 -0400

    Update docs.js

--- beta/js/docs.js
+++ beta/js/docs.js
@@ -79,8 +79,8 @@ function docReader() {
   document.getElementById('tidocs-header').remove();
 }
 
-window.addEventListener('DOMContentLoaded', function() {
+/*window.addEventListener('DOMContentLoaded', function() {
   if (localStorage.editAutoSave == undefined) {
     localStorage.removeItem('tidocssave');
   }
-}, false);
+}, false);*/
