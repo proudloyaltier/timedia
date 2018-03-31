7479122c990905b5af43289b997af243c88caac7
proudloyaltier (plt) <timediamail@gmail.com>
2018-03-31 17:20:56 -0400

    Update slides.js

--- beta/js/slides.js
+++ beta/js/slides.js
@@ -72,7 +72,7 @@ function addImage(src) {
 
 function addSlide() {
   slideshow.push(" <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>");
-  current_slide = localStorage.slideshow.length - 1;
+  current_slide = slideshow.length - 1;
   updateSlide();
 }
 
