1a9131b0e1cf29889502578ecf75b249fe97f80c
proudloyaltier (plt) <timediamail@gmail.com>
2018-04-08 17:28:46 -0400

    Update messages.js

--- beta/js/messages.js
+++ beta/js/messages.js
@@ -23,17 +23,20 @@ function getMessages() {
       if (child.key > 0) {
       var length = child.key + 1
       } else {
-      var length = 1
+      var length = 0
       }
-      document.getElementById('private-messages').innerHTML = '<p>' + CryptoJS.AES.decrypt(child.val(), localStorage.password).toString(CryptoJS.enc.Utf8); + '</p><br>' + document.getElementById('private-messages').innerHTML;
+      var currentHTML = document.getElementById('private-messages').innerHTML
+      document.getElementById('private-messages').innerHTML = '<p>' + CryptoJS.AES.decrypt(child.val(), localStorage.password).toString(CryptoJS.enc.Utf8); + '</p><br>' + currentHTML;
     });
   });
 }
 
 function sendMessage(message) {
   if (length !== undefined) {
-  window.dbRef.child(getQueryVariable("app")).child(length + 1).set(CryptoJS.AES.encrypt("<b>" + localStorage.name + "</b>" + " said:" + "<br>" + message, localStorage.password) + "");
+  window.dbRef.child(getQueryVariable("app")).child(length).set(CryptoJS.AES.encrypt("<b>" + localStorage.name + "</b>" + " said:" + "<br>" + message, localStorage.password) + "");
+  length = length + 1
   } else {
   window.dbRef.child(getQueryVariable("app")).child(0).set(CryptoJS.AES.encrypt("<b>" + localStorage.name + "</b>" + " said:" + "<br>" + message, localStorage.password) + "");
+  length = 1
   }
 }
