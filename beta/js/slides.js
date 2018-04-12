85c25b57dbab9a4cb5f5e62dba024da2813d0ad6
proudloyaltier (plt) <timediamail@gmail.com>
2018-04-12 11:30:57 -0400

    Update slides.js

--- beta/js/slides.js
+++ beta/js/slides.js
@@ -1,3 +1,26 @@
+function loadSlides() {
+  if (getQueryVariable("s") == false) {
+    localStorage.removeItem('tislidessave');
+  } else {
+      var urlRef = window.dbRef.child(getQueryVariable("s"));
+      urlRef.on("value", function (snapshot) {
+      snapshot.forEach(function (child) {
+      localStorage.owner = child.key;
+      if (localStorage.owner.toLowerCase() !== localStorage.name.toLowerCase()) {
+        window.location.href = 'index.html?app=7';
+        alert("Access Denied! Get TIed!")
+      }
+      localStorage.tislidessave = getQueryVariable("s");
+      slideshow = child.val()
+      updateSlide();
+      for (var i = 0; i < document.getElementsByClassName('edit-slides').length; i++) {
+        document.getElementsByClassName('edit-slides')[i].onmousedown = function() {beginDrag(this);};
+      }
+    });
+  });
+ }
+}
+
 if (getQueryVariable("s") == false) {
   var slideshow = ["<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>"];
 }
@@ -175,26 +198,7 @@ function presentSlide() {
 }
 
 window.addEventListener('DOMContentLoaded', function () {
-  if (getQueryVariable("s") == false) {
-    localStorage.removeItem('tislidessave');
-  } else {
-      var urlRef = window.dbRef.child(getQueryVariable("s"));
-      urlRef.on("value", function (snapshot) {
-      snapshot.forEach(function (child) {
-      localStorage.owner = child.key;
-      if (localStorage.owner.toLowerCase() !== localStorage.name.toLowerCase()) {
-        window.location.href = 'index.html?app=7';
-        alert("Access Denied! Get TIed!")
-      }
-      localStorage.tislidessave = getQueryVariable("s");
-      slideshow = child.val()
-      updateSlide();
-      for (var i = 0; i < document.getElementsByClassName('edit-slides').length; i++) {
-        document.getElementsByClassName('edit-slides')[i].onmousedown = function() {beginDrag(this);};
-      }
-    });
-  });
- }
+  loadSlides()
 }, false);
 
 if (localStorage.slidesMobile !== undefined) {
