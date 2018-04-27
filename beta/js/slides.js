37ee6d93483a7486e7444a70b0f45740db3e05c7
proudloyaltier (plt) <timediamail@gmail.com>
2018-04-27 14:50:38 -0400

    Update slides.js

--- beta/js/slides.js
+++ beta/js/slides.js
@@ -123,7 +123,7 @@ function saveSlide() {
   slideshow[current_slide] = slideContainer.innerHTML;
   if (localStorage.tislidessave == undefined) {
     var tislidessave = generateRandString()
-    storeInDatabase(tislidessave, slideshow);
+    storeInDatabase(tislidessave, CryptoJS.AES.encrypt(slideshow + "", localStorage.password) + ""));
     localStorage.tislidessave = tislidessave;
     var urlRef = window.dbRef.child(tislidessave);
     urlRef.on("value", function (snapshot) {
@@ -136,7 +136,7 @@ function saveSlide() {
     localStorage.workToSaveTitle = document.getElementById('slidesTitle').value;
     localStorage.workToSave = url;
   } else {
-    window.dbRef.child(localStorage.tislidessave).child(localStorage.owner).set(slideshow);
+    window.dbRef.child(localStorage.tislidessave).child(localStorage.owner).set(CryptoJS.AES.encrypt(slideshow + "", localStorage.password) + ""));
     var tislidessave = localStorage.tislidessave
   }
 }
@@ -198,7 +198,8 @@ window.addEventListener('DOMContentLoaded', function () {
       }
       document.getElementById('slidesTitle').remove();
       localStorage.tislidessave = getQueryVariable("s");
-      slideshow = child.val()
+      slideshow1 = CryptoJS.AES.decrypt(child.val(), localStorage.password).toString(CryptoJS.enc.Utf8);
+      slideshow = slideshow1.split(",")
       updateSlide();
       for (var i = 0; i < document.getElementsByClassName('edit-slides').length; i++) {
         document.getElementsByClassName('edit-slides')[i].onmousedown = function() {beginDrag(this);};
