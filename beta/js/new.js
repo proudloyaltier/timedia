b5f3d26f4164e85af051e80ee33f7188191272b5
proudloyaltier (plt) <timediamail@gmail.com>
2018-05-02 14:50:21 -0400

    Update new.js

--- beta/js/new.js
+++ beta/js/new.js
@@ -1,12 +1,12 @@
 function submitApp() {
-  var appName = document.getElementById('newapp-name').value;
-  var appSource = document.getElementById('newapp-source').value;
-  var appIcon = document.getElementById('newapp-icon').value;
-  var formURL = 'https://docs.google.com/forms/d/e/1FAIpQLSdDKfLkPKG5bjNEMv_gDC221zTLDAuF0hUCKbadSLZnpFYVrw/viewform?entry.546288400=';
-  var data = btoa('app_name:' + appName + ',created_by:' + localStorage.getItem('name') + ',icon:' + appIcon + ',source:' + appSource);
-  window.location.href = formURL + data
+  var appName = document.getElementById("newapp-name").value;
+  var appSource = document.getElementById("newapp-source").value;
+  var appIcon = document.getElementById("newapp-icon").value;
+  var formURL = "https://docs.google.com/forms/d/e/1FAIpQLSdDKfLkPKG5bjNEMv_gDC221zTLDAuF0hUCKbadSLZnpFYVrw/viewform?entry.546288400=";
+  var data = btoa("app_name:" + appName + ",created_by:" + localStorage.getItem("name") + ",icon:" + appIcon + ",source:" + appSource);
+  window.location.href = formURL + data;
 }
 
 function updateIcon() {
-  document.getElementById('newapp-preview').innerHTML = 'Preview: <span class="glyphicon glyphicon-' + document.getElementById('newapp-icon').value + '"></span>'
+  document.getElementById("newapp-preview").innerHTML = "Preview: <span class="glyphicon glyphicon-" + document.getElementById("newapp-icon").value + ""></span>"
 }
