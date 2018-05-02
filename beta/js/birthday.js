d029e3b02b5f91cd6b872decc88ea74fa5757067
proudloyaltier (plt) <timediamail@gmail.com>
2018-05-02 14:27:00 -0400

    Update birthday.js

--- beta/js/birthday.js
+++ beta/js/birthday.js
@@ -4,7 +4,7 @@ var year = date.getYear();
 var month = date.getMonth() + 1;
 var day = date.getDate();
 
-if (month == 5 && day == 2) {
+if (month == 5 && day == 9) {
 
   document.getElementById("tiBar").style["background-image"] = 'url("https://media.giphy.com/media/120ErahsQyf1q8/giphy.gif")';
 }
