ac1bdeddda1d1d30ffc100b34b3b6cb7d5ab46b1
proudloyaltier (plt) <timediamail@gmail.com>
2018-05-02 14:25:01 -0400

    Create birthday.js

---
+++ beta/js/birthday.js
@@ -0,0 +1,10 @@
+var date = new Date();
+
+var year = date.getYear();
+var month = date.getMonth() + 1;
+var day = date.getDate();
+
+if (month == 5 && day == 2) {
+
+  document.getElementById("tiBar").style["background-image"] = 'url("https://media.giphy.com/media/120ErahsQyf1q8/giphy.gif")';
+}
