b7333a3229298f133d6352a13d9eefa5d765531e
proudloyaltier (plt) <timediamail@gmail.com>
2018-05-02 14:53:55 -0400

    MAJOR CLEANUPS

--- beta/js/settings.js
+++ beta/js/settings.js
@@ -1,15 +1,15 @@
 function updateSettings() {
-  localStorage.bgImg = document.getElementById('backgroundImg').value;
-  localStorage.barColor = document.getElementById('barColor').value;
+  localStorage.bgImg = document.getElementById("backgroundImg").value;
+  localStorage.barColor = document.getElementById("barColor").value;
   location.reload();
 }
 
 if (localStorage.bgImg !== undefined) {
-  document.getElementById('body').background = localStorage.bgImg;
+  document.getElementById("body").background = localStorage.bgImg;
 }
 
 if (localStorage.barColor !== undefined) {
-  document.getElementById('timedia-nav-bar').style = "background-color: #" + localStorage.barColor + ";";
+  document.getElementById("timedia-nav-bar").style = "background-color: #" + localStorage.barColor + ";";
 }
 
 function lastTile() {
@@ -21,28 +21,28 @@ document.getElementById("personalizationSettings").style = "display: none";
 document.getElementById("tileSettings").style = "display: none";
 
 function sstismile() {
-  if (document.getElementById("tismileSettings").style == "display: block") {
-    document.getElementById("tismileSettings").style = "display: none";
+  if (document.getElementById("tismileSettings").style.display == "") {
+    document.getElementById("tismileSettings").style.display = "";
     document.getElementById("sstismile-icon").className = "glyphicon glyphicon-menu-down";
   } else {
-    document.getElementById("tismileSettings").style = "display: block";
+    document.getElementById("tismileSettings").style.display = "";
     document.getElementById("sstismile-icon").className = "glyphicon glyphicon-menu-up";
   }
 }
 
 function sstiles() {
-  document.getElementById("tismileSettings").style = "display: none";
-  document.getElementById("personalizationSettings").style = "display: none";
-  document.getElementById("tileSettings").style = "display: block";
+  document.getElementById("tismileSettings").style.display = "none";
+  document.getElementById("personalizationSettings").style.display = "none";
+  document.getElementById("tileSettings").style.display = "";
   document.getElementById("sspersonal-icon").className = "glyphicon glyphicon-menu-down";
   document.getElementById("sstiles-icon").className = "glyphicon glyphicon-menu-up";
   document.getElementById("sstismile-icon").className = "glyphicon glyphicon-menu-down";
 }
 
 function sspersonal() {
-  document.getElementById("tismileSettings").style = "display: none";
-  document.getElementById("personalizationSettings").style = "display: block";
-  document.getElementById("tileSettings").style = "display: none";
+  document.getElementById("tismileSettings").style.display = "none";
+  document.getElementById("personalizationSettings").style.display = "";
+  document.getElementById("tileSettings").style.display = "none";
   document.getElementById("sspersonal-icon").className = "glyphicon glyphicon-menu-up";
   document.getElementById("sstiles-icon").className = "glyphicon glyphicon-menu-down";
   document.getElementById("sstismile-icon").className = "glyphicon glyphicon-menu-down";
@@ -51,7 +51,6 @@ function sspersonal() {
 function darkMode() {
   body.className = "dark-mode";
 }
-
 
 function darkModeOff() {
   localStorage.darkmode = false;
@@ -63,9 +62,9 @@ function setDarkMode() {
   window.location.reload();
 }
 
-if (localStorage.darkmode == "true") {
+if (localStorage.getItem("darkmode") == "true") {
   darkMode();
-  document.getElementById('dark-mode-on').remove();
+  document.getElementById("dark-mode-on").remove();
 } else {
-  document.getElementById('dark-mode-off').remove();
+  document.getElementById("dark-mode-off").remove();
 }
