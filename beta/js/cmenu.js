9d17598f37cb27db6eebe56c913c41edfb84852a
proudloyaltier (plt) <timediamail@gmail.com>
2018-05-02 14:32:16 -0400

    MAJOR CLEANUPS

--- beta/js/cmenu.js
+++ beta/js/cmenu.js
@@ -2,22 +2,27 @@ var normalMenu = document.getElementById("context-menu").innerHTML;
 var specialElement = false;
 var overCmenu = false;
 
-document.getElementById("context-menu").mouseover = function() {overCmenu = true;};
-document.getElementById("context-menu").mouseout = function() {overCmenu = false;};
+document.getElementById("context-menu").mouseover = function() {
+  overCmenu = true;
+};
+document.getElementById("context-menu").mouseout = function() {
+  overCmenu = false;
+};
 
 function openCmenu(e) {
   e = e || window.event;
   e.preventDefault();
-  if(!overTile && !specialElement) {
+  if (!overTile && !specialElement) {
     document.getElementById("context-menu").innerHTML = normalMenu;
   }
   document.getElementById("context-menu").style = "display: inline-block";
-  document.getElementById("context-menu").style.top = ( e.clientY-10) + "px";
+  document.getElementById("context-menu").style.top = (e.clientY - 10) + "px";
   document.getElementById("context-menu").style.left = e.clientX + "px";
   document.getElementById("tiri-mini-box").focus();
 }
+
 function closeCmenu() {
-  if(!overCmenu) {
+  if (!overCmenu) {
     document.getElementById("context-menu").style = "display: none";
   }
 }
@@ -25,8 +30,12 @@ window.addEventListener("contextmenu", openCmenu);
 window.addEventListener("click", closeCmenu);
 
 function initSpecialElement(element) {
-  element.mouseover = function() {specialElement = true;};
-  element.mouseout = function() {specialElement = false;};
+  element.mouseover = function() {
+    specialElement = true;
+  };
+  element.mouseout = function() {
+    specialElement = false;
+  };
 }
 
 //TILE CODE
@@ -47,5 +56,9 @@ document.addEventListener("mousedown", mouseDown);
 document.addEventListener("mouseup", mouseUp);
 
 if (getQueryVariable("app") == 7) {
-setInterval(function() {if(!overTile) {mouseUp();}}, 0);
+  setInterval(function() {
+    if (!overTile) {
+      mouseUp();
+    }
+  }, 0);
 }
