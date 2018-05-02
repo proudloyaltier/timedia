ab7947f69b395fac81820b2d6004fc15ed6f9f34
proudloyaltier (plt) <timediamail@gmail.com>
2018-05-02 14:43:38 -0400

    MAJOR CLEANUPS

--- beta/js/login-tismile.js
+++ beta/js/login-tismile.js
@@ -1,8 +1,8 @@
-var pixelation = 100 - Number(localStorage.security);
+var pixelation = 100 - Number(localStorage.getItem("security"));
 var imgObj = new Image();
-var canvas = document.getElementById('tismile-canvas');
-var context = canvas.getContext('2d');
-var video = document.getElementById('tismile-video');
+var canvas = document.getElementById("tismile-canvas");
+var context = canvas.getContext("2d");
+var video = document.getElementById("tismile-video");
 
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia({
@@ -14,30 +14,28 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 }
 
 if (localStorage.faces !== undefined) {
-  document.getElementById('login-user').style = "display: none;";
-  document.getElementById('login-tismile').style = "";
+  document.getElementById("login-user").style.display = "none";
+  document.getElementById("login-tismile").style.display = "";
 }
 
 function loginPassword() {
-  document.getElementById('login-tismile').style = "display: none;";
-  document.getElementById('login-user').style = "";
-  document.getElementById('login-username').value = localStorage.tismileaccount;
-  document.getElementById('login-password').focus();
+  document.getElementById("login-tismile").style.display = "none";
+  document.getElementById("login-user").style.display = "";
+  document.getElementById("login-username").value = localStorage.getItem("tismileaccount");
+  document.getElementById("login-password").focus();
 }
 
 function checkFaces() {
   context.drawImage(video, 0, 0, 640, 480);
   pixelate(context, 640, 480, 0, 0);
   window.faceToCompare = canvas.toDataURL();
-  
+
   if (localStorage.faces == faceToCompare) {
-    console.log("Unlocked!");
     firebase.auth().signInWithEmailAndPassword(localStorage.tismileaccount + "@timediatied.com", localStorage.tismileaccountpassword)
   }
 }
 
 function pixelate(context, srcWidth, srcHeight, xPos, yPos) {
-
   var sourceX = xPos,
     sourceY = yPos,
     imageData = context.getImageData(sourceX, sourceY, srcWidth, srcHeight),
@@ -62,7 +60,6 @@ function pixelate(context, srcWidth, srcHeight, xPos, yPos) {
     }
   }
 
-  // overwrite original image
   context.putImageData(imageData, xPos, yPos);
   pixelation -= 1;
 }
