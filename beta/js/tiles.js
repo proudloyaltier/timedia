f01bee4ec3afd1d75cb3fe54584b0bc580fd04a9
proudloyaltier (plt) <timediamail@gmail.com>
2018-05-12 09:34:35 -0400

    Update tiles.js

--- beta/js/tiles.js
+++ beta/js/tiles.js
@@ -274,6 +274,7 @@ function loadTiles() {
         if (localStorage.tileDeleteButton !== "true") {
           document.getElementById('tiles-tiles').innerHTML += '<li onmouseover="overTile = true" onmouseout="overTile = false" oncontextmenu="openTileContext(' + i + ')" style="float: left; width: 250px; height: 250px;" class="card" onclick="window.open(\'' + localStorage.files.split(",")[i].split("!!")[1] + '\');"><h3><center>' + localStorage.files.split(",")[i].split("!!")[0] + '<br><span style="font-size: 300%; color: #ff0000;" class="glyphicon glyphicon-bookmark"><br></span><br></center></h3></span></li>';
         }
+      }
       if (localStorage.files.split(",")[i].split("!!")[1].includes("?app=1")) {
         if (localStorage.tileDeleteButton == "true") {
           document.getElementById("tiles-tiles").innerHTML += '<li onmouseover="overTile = true" onmouseout="overTile = false" oncontextmenu="openTileContext(' + i + ')" style="float: left; width: 250px; height: 250px;" class="card" onclick="window.open(\'' + localStorage.files.split(",")[i].split("!!")[1] + '\');"><h3><center>' + localStorage.files.split(",")[i].split("!!")[0] + '<br><span style="font-size: 300%; color: #f754f9;" class="glyphicon glyphicon-picture"><br></span><br><br><button class="btn btn-danger" id="delete-single-tile" style="" onclick="deleteTile(' + i + ');">Delete</button></center></h3></span></li>';
