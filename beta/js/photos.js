a9fba34c450c0eff1108de2c25f0f24389a95c11
proudloyaltier (plt) <timediamail@gmail.com>
2018-05-15 10:04:23 -0400

    Update photos.js

--- beta/js/photos.js
+++ beta/js/photos.js
@@ -37,7 +37,7 @@ function convertPhoto() {
 }
 
 function viewPhotos(i) {
-var urlRef = window.dbRef.child(localStorage.files.split(",")[i].split("!!")[1].split("&i=")[1]);
+var urlRef = window.dbRef.child(localStorage.files.split(",")[i].split("&i=")[1]);
   urlRef.on("value", function (snapshot) {
     snapshot.forEach(function (child) {
       localStorage.owner = child.key;
@@ -58,7 +58,7 @@ var urlRef = window.dbRef.child(localStorage.files.split(",")[i].split("!!")[1].
 }
 
 function thumbPhotos(i) {
-var urlRef = window.dbRef.child(localStorage.files.split(",")[i].split("!!")[1].split("&i=")[1]);
+var urlRef = window.dbRef.child(localStorage.files.split(",")[i].split("&i=")[1]);
   urlRef.on("value", function (snapshot) {
     snapshot.forEach(function (child) {
       var contentsrc = CryptoJS.AES.decrypt(child.val(), localStorage.password).toString(CryptoJS.enc.Utf8);
