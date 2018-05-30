4903bc962563aee3be77dd10a569535b1e645c6e
proudloyaltier <timediamail@gmail.com>
2018-05-30 12:45:09 -0400

    Update settings.js

--- beta/js/settings.js
+++ beta/js/settings.js
@@ -68,3 +68,66 @@ if (localStorage.getItem("darkmode") == "true") {
 } else {
   document.getElementById("dark-mode-off").remove();
 }
+
+document.getElementById("settings-back-arrow").innerHTML = "<";
+
+var settingsPage = "settings";
+
+var settings = {
+  settings: {
+    Account: '<h6 style="font-size: 150%">Change Password </h6><input class="form-control" type="text" id="passchange1" placeholder="New Password"><input class="form-control" type="text" id="passchange2" placeholder="Confirm New Password"><button onclick="changePasswordButton();" class="btn btn-primary">Change Password</button><br>',
+    Tiles: '<button onclick="backupTiles(' + TilesBackup.txt + ', localStorage.files)" class="btn btn-primary">Backup Tiles</button><button onclick="restoreTiles()" class="btn btn-primary">Restore Tiles</button><br><br><button onclick="lastTile()" class="btn btn-success">Open The Last Opened Tile</button><br><br><button onclick="resetTiles()" class="btn btn-danger">Reset Tiles</button><br>',
+    Personalization: '<h6 style="font-size: 150%"> Theming Options </h6> Background Image<br><input class="form-control" type="text" id="backgroundImg"><br> Bar Color<br><input style="width: 5%;" id="barColor" class="jscolor form-control" value="2296f3"><br><button onclick="updateSettings()" class="btn btn-success">Save</button><br><br><button onclick="setDarkMode();" class="btn btn-primary" id="dark-mode-on">Turn On Dark Mode</button><button onclick="darkModeOff();" class="btn btn-primary" id="dark-mode-off">Turn Off Dark Mode</button>'
+  }
+};
+
+function changeSettingsPage() {
+  var pageOn = settings;
+  for (var i = 0; i < settingsPage.split("/").length; i++) {
+    pageOn = pageOn[settingsPage.split("/")[i]];
+  }
+  
+  if ((typeof pageOn).toLowerCase() == "object") {
+    document.getElementById('settings-sections').innerHTML = "";
+    for (var i = 0; i < Object.keys(pageOn).length; i++) {
+      var newSection = document.createElement("div");
+      newSection.innerHTML = Object.keys(pageOn)[i];
+      newSection.key = Object.keys(pageOn)[i];
+      newSection.onclick = function() {
+        settingsPage += "/" + this.key;
+        changeSettingsPage();
+      };
+      document.getElementById('settings-sections').appendChild(newSection);
+    }
+  } else {
+    document.getElementById('settings-sections').innerHTML = pageOn;
+  }
+  if(settingsPage == "settings") {
+    document.getElementById("settings-back-arrow").style.display = "none";
+  } else {
+    document.getElementById("settings-back-arrow").style.display = "block";
+  }
+}
+
+function backSettings() {
+  settingsPage = settingsPage.replace("/" + settingsPage.split("/")[settingsPage.split("/").length - 1], "");
+  changeSettingsPage();
+}
+
+function closeSettings() {
+  document.getElementById("settings").style.right = "-251px";
+}
+
+function openSettings() {
+  document.getElementById("settings").style.right = "0px";
+}
+
+function toggleSettings() {
+  if(parseInt(document.getElementById("settings").style.right) == 0) {
+    closeSettings();
+  } else {
+    openSettings();
+  }
+}
+
+changeSettingsPage();
