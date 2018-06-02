745496d0fef43dae9845b7a22c657e0b5708d68a
proudloyaltier <timediamail@gmail.com>
2018-06-02 10:48:28 -0400

    Create titanium.js

---
+++ beta/js/titanium.js
@@ -0,0 +1,7 @@
+if ((!navigator.userAgent.toLowerCase().includes("titanium")) && localStorage.dismissedTiTanium !== true) {
+  alertify.confirm("It appears that you are not using TiTanium. Switch today for a faster, more private browser!", function () {
+    window.location.href = "https://proudloyaltier.github.io/titanium";
+  }, function() {
+    localStorage.dismissedTiTanium = true;
+  });
+}
