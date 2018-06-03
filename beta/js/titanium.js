3cc5f05af1d797a1bc566404566045d9d481de02
proudloyaltier <timediamail@gmail.com>
2018-06-03 07:35:56 -0400

    Update titanium.js

--- beta/js/titanium.js
+++ beta/js/titanium.js
@@ -1,5 +1,5 @@
 if (!navigator.userAgent.toLowerCase().includes("titanium") && localStorage.dismissedTiTanium !== "true") {
-  alertify.log("It appears that you are not using TiTanium. Switch today for a faster, more private browser!", function(ev) {
+  alertify.delay(20000).log("It appears that you are not using TiTanium. Switch today for a faster, more private browser!", function(ev) {
     ev.preventDefault();
     window.location.href = "https://proudloyaltier.github.io/titanium";
     localStorage.dismissedTiTanium = true;
