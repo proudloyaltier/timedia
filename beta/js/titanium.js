76fa389b5079727aa9237230bad3b7eb075b38e5
proudloyaltier <timediamail@gmail.com>
2018-06-03 07:52:50 -0400

    Update titanium.js

--- beta/js/titanium.js
+++ beta/js/titanium.js
@@ -1,7 +1,9 @@
-if (!navigator.userAgent.toLowerCase().includes("titanium") && localStorage.dismissedTiTanium !== "true") {
-  alertify.delay(20000).log("It appears that you are not using TiTanium. Switch today for a faster, more private browser!", function(ev) {
-    ev.preventDefault();
-    window.location.href = "https://proudloyaltier.github.io/titanium";
-    localStorage.dismissedTiTanium = true;
- })
+function openTiTaniumDownload() {
+window.open('https://proudloyaltier.github.io/titanium');
 }
+
+var titaniumad = document.createElement('div')
+titaniumad.onclick = openTiTaniumDownload();
+titaniumad.style = "top: 0; width: 100%; background-color: black; border-radius: 2px; color: white; text-align: center; height: 30px;"
+titaniumad.innerText = "It appears that you are not using TiTanium. Switch today for a faster, more private browser!";
+document.body.insertBefore(titaniumad, document.body.firstChild)
