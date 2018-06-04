14ad48ae8fde95140e80de83470dd55427f01a5c
proudloyaltier <timediamail@gmail.com>
2018-06-04 08:01:13 -0400

    Create download.js

---
+++ beta/titanium/download.js
@@ -0,0 +1,33 @@
+var isWindows = navigator.platform.toLowerCase().includes("win");
+var isMac = navigator.platform.toLowerCase().includes("mac");
+var isLinux = navigator.platform.toLowerCase().includes("linux");
+
+var downloadButton = document.getElementById("downloadButton");
+var availableDownloads = document.getElementById("availableDownloads");
+
+var windowsDownload = "#";
+var macDownload = "#";
+var linuxDownload = "#";
+
+if (isWindows) {
+  downloadButton.innerText = "Download for Windows";
+} else if (isMac) {
+  downloadButton.innerText = "Download for Mac";
+} else if (isLinux) {
+  downloadButton.innerText = "Download for Linux";
+} else {
+  downloadButton.style.display = "none";
+  availableDownloads.style.display = "";
+}
+
+function download() {  
+  if (isWindows) {
+    window.location.replace(windowsDownload);
+  } else if (isMac) {
+    window.location.replace(macDownload);
+  } else if (isLinux) {
+    window.location.replace(linuxDownload);
+  } else {
+    alert("TiTanium is not yet supported on your device.");
+  }
+}
