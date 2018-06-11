32b4e6aa138e905559f247bc5fdeeb78347ef103
proudloyaltier <timediamail@gmail.com>
2018-06-11 14:51:25 -0400

    tiapp beta extensions file

--- beta/js/new.js
+++ beta/js/new.js
@@ -2,9 +2,11 @@ function submitApp() {
   var appName = document.getElementById("newapp-name").value;
   var appSource = document.getElementById("newapp-source").value;
   var appIcon = document.getElementById("newapp-icon").value;
-  var formURL = "https://docs.google.com/forms/d/e/1FAIpQLSdDKfLkPKG5bjNEMv_gDC221zTLDAuF0hUCKbadSLZnpFYVrw/viewform?entry.546288400=";
   var data = btoa("app_name:" + appName + ",created_by:" + localStorage.getItem("name") + ",icon:" + appIcon + ",source:" + appSource);
-  window.location.href = formURL + data;
+  var a = document.createElement("a");
+  a.href = "data:application/octet-stream;charset=utf-8;base64," + data;
+  a.download = appName + ".tiapp";
+  a.click();
 }
 
 function updateIcon() {
