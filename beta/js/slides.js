eba46990e2d43a9e29d841520bc30da4869e4d66
proudloyaltier <timediamail@gmail.com>
2018-06-25 19:30:11 -0400

    Useless! Doesn't work

--- beta/js/slides.js
+++ beta/js/slides.js
@@ -87,9 +87,6 @@ function addImage() {
         specialElement = false
         };
         image.className += ' slides-img';
-        image.oncontextmenu = function () {
-        document.getElementById("context-menu").innerHTML = "<ul class='context-menu__items'><li><a href='#' onclick='document.getElementsByClassName(\"slides-img\")[" + document.getElementsByClassName("slides-img").length + "].onmousedown = null;'>Lock Position <span class='glyphicon glyphicon-lock'></span></a></li></ul>";
-        };
        slideContainer.appendChild(image);
        saveSlide();
      });
