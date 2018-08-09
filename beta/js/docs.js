69bc9e2c67efd4ba46a9d96ab491fa3d66fe7174
proudloyaltier <timediamail@gmail.com>
2018-08-09 10:36:10 -0400

    Remove jQuery and CLEANUPS

--- beta/js/docs.js
+++ beta/js/docs.js
@@ -1,10 +1,12 @@
-var isFirefox = typeof InstallTrigger !== 'undefined';
+var tidocsContent = document.getElementsById("tidocsContent");
+var tidocsTitle = document.getElementById("tidocsTitle");
+
+var isFirefox = typeof InstallTrigger !== "undefined";
 var isIE = /*@cc_on!@*/ false || !!document.documentMode;
 
 function tiPrint() {
   if (isFirefox || isIE) {
     if (isFirefox) {
-
       if (localStorage.setupOneClick !== "true") {
         swal.setDefaults({
           confirmButtonText: "Next &rarr;",
@@ -39,43 +41,37 @@ function tiPrint() {
   }
 }
 
-$(document).on("keydown", function(e) {
-  if ((e.ctrlKey || e.metaKey) && (e.key == "p" || e.charCode == 16 || e.charCode == 112 || e.keyCode == 80)) {
+document.addEventListener("keydown", function(e) {
+  if ((e.ctrlKey || e.metaKey) && (e.charCode === 80)) {
     tiPrint();
     e.cancelBubble = true;
     e.preventDefault();
-
     e.stopImmediatePropagation();
   }
 });
 
-if (isFirefox || isIE) {
-  $("#legacyPrintButton").hide();
-  $("#printButton").show();
-}
-
 function saveDoc() {
   if (localStorage.tidocssave == undefined) {
     var tidocssave = generateRandString()
-    window.dbRef.child("docs").child(tidocssave).child(localStorage.name).set(CryptoJS.AES.encrypt(document.getElementById('tidocsContent').innerHTML, localStorage.password) + "");
+    window.dbRef.child("docs").child(tidocssave).child(localStorage.name).set(CryptoJS.AES.encrypt(tidocsContent.innerHTML, localStorage.password).toString());
+
     localStorage.tidocssave = tidocssave;
     var urlRef = window.dbRef.child("docs").child(tidocssave);
-    urlRef.on("value", function (snapshot) {
-      snapshot.forEach(function (child) {
+    urlRef.on("value", function(snapshot) {
+      snapshot.forEach(function(child) {
         localStorage.owner = child.key;
       });
     });
-    var url = "index.html?app=3" + '&p=' + tidocssave;
+
+    var url = "index.html?app=3&p=" + tidocssave;
     localStorage.recentUrl = url;
-    localStorage.workToSaveTitle = document.getElementById('docsTitle').value;
+    localStorage.workToSaveTitle = tidocsTitle.value;
     localStorage.workToSave = url;
   } else {
     window.dbRef.child("docs").child(localStorage.tidocssave).child(localStorage.owner).set(CryptoJS.AES.encrypt(document.getElementById('tidocsContent').innerHTML, localStorage.password) + "");
     var tidocssave = localStorage.tidocssave
   }
 }
-
-var tidocsContent = document.getElementsByClassName("tidocsContent")[0];
 
 if ("addEventListener" in tidocsContent) {
   tidocsContent.addEventListener("keyup", saveDoc, false);
@@ -87,34 +83,6 @@ if ("addEventListener" in tidocsContent) {
   }
 }
 
-
-if (getQueryVariable("p") !== false || localStorage.edit !== undefined) {
-  if (localStorage.edit !== undefined) {
-    document.getElementById('docsTitle').remove();
-    document.getElementById('tidocsContent').innerHTML = localStorage.edit;
-    localStorage.editAutoSave = localStorage.edit;
-    localStorage.removeItem('edit');
-    throw new Error("Opened edit.");
-  }
-
-  document.getElementById('create').remove();
-  document.getElementById('view').style.display = "block";
-  document.getElementById('tidocs-edit').style.display = "block";
-  document.getElementById('tidocs-reader').style.display = "block";
-  var urlRef = window.dbRef.child("docs").child(getQueryVariable("p"));
-  urlRef.on("value", function (snapshot) {
-    snapshot.forEach(function (child) {
-      localStorage.owner = child.key;
-      if (localStorage.owner.toLowerCase() !== localStorage.name.toLowerCase()) {
-        window.location.href = 'index.html?app=7';
-        alert("Access Denied! Get TIed!")
-      }
-      document.getElementById('view').innerHTML = CryptoJS.AES.decrypt(child.val(), localStorage.password).toString(CryptoJS.enc.Utf8);
-      window.edit = document.getElementById('view').innerHTML
-    });
-  });
-}
-
 function editDoc() {
   localStorage.edit = window.edit;
   window.location.href = "index.html?app=3";
@@ -122,14 +90,45 @@ function editDoc() {
 }
 
 function docReader() {
-  document.getElementById('timedia-nav-bar').remove();
-  document.getElementById('tidocs-edit').remove();
-  document.getElementById('tidocs-reader').remove();
-  document.getElementById('tidocs-header').remove();
+  document.getElementById("timedia-nav-bar").style.display = "none";
+  document.getElementById("tidocs-edit").style.display = "none";
+  document.getElementById("tidocs-reader").style.display = "none";
+  document.getElementById("tidocs-header").style.display = "none";
 }
 
-window.addEventListener('DOMContentLoaded', function () {
-  if (localStorage.editAutoSave == undefined) {
-    localStorage.removeItem('tidocssave');
+if (typeof localStorage.editAutoSave === "undefined") {
+  localStorage.removeItem("tidocssave");
+}
+
+if (getQueryVariable("p") !== false || localStorage.edit !== undefined) {
+  if (localStorage.edit !== undefined) {
+    tidocsTitle.style.display = "none";
+    tidocsContent.innerHTML = localStorage.edit;
+    localStorage.editAutoSave = localStorage.edit;
+    localStorage.removeItem("edit");
+  } else {
+    document.getElementById("create").style.display = "none";
+    document.getElementById("view").style.display = "";
+    document.getElementById("tidocs-edit").style.display = "";
+    document.getElementById("tidocs-reader").style.display = "";
+
+    var urlRef = window.dbRef.child("docs").child(getQueryVariable("p"));
+    urlRef.on("value", function(snapshot) {
+      snapshot.forEach(function(child) {
+        localStorage.owner = child.key;
+
+        if (localStorage.owner.toLowerCase() !== localStorage.name.toLowerCase()) {
+          window.location.href = "index.html?app=7";
+        }
+
+        document.getElementById("view").innerHTML = CryptoJS.AES.decrypt(child.val(), localStorage.password).toString(CryptoJS.enc.Utf8);
+        window.edit = document.getElementById("view").innerHTML;
+      });
+    });
   }
-}, false);
+}
+
+if (isFirefox || isIE) {
+  document.getElementById("legacyPrintButton").style.display = "none";
+  document.getElementById("printButton").style.display = "";
+}
