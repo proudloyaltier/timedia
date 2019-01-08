1ef246f55ff66921cbefe3c12eca6909e9f9d64b
proudloyaltier <timediamail@gmail.com>
2019-01-08 15:04:26 -0500

    Adds new font change

--- beta/js/fontChange.js
+++ beta/js/fontChange.js
@@ -22,6 +22,25 @@
    *
    */
 
+  function setEndOfContenteditable(contentEditableElement) {
+    var range, selection;
+    if (document.createRange)//Firefox, Chrome, Opera, Safari, IE 9+
+    {
+      range = document.createRange();//Create a range (a range is a like the selection but invisible)
+      range.selectNodeContents(contentEditableElement);//Select the entire contents of the element with the range
+      range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+      selection = window.getSelection();//get the selection object (allows you to change selection)
+      selection.removeAllRanges();//remove any selections already made
+      selection.addRange(range);//make the range you have just created the visible selection
+    }
+    else if (document.selection)//IE 8 and lower
+    {
+      range = document.body.createTextRange();//Create a range (a range is a like the selection but invisible)
+      range.moveToElementText(contentEditableElement);//Select the entire contents of the element with the range
+      range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+      range.select();//Select the range (make it the visible selection
+    }
+  }
 
   (function($) {
 
@@ -165,6 +184,7 @@
       'initial': 'Courier New,Courier New,Courier,monospace',
       'selected': function(style) {
        $('#tidocsContent').focus()
+       setEndOfContenteditable(document.getElementById("tidocsContent"));
        document.execCommand('styleWithCSS',false,true);
        document.execCommand('fontName',false,style);
       },
