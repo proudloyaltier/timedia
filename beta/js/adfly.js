0425f15d8588dc89f90497b507ef72ee0e4381ab
proudloyaltier <timediamail@gmail.com>
2019-01-18 13:39:08 -0500

    Update adfly.js

--- beta/js/adfly.js
+++ beta/js/adfly.js
@@ -7,12 +7,10 @@ Copyright (C) 2019 The TiMedia Team. All rights reserved.
 
 */
 
-// Variables
+if (window.self === window.top) {
+  var websiteLinks = document.getElementsByTagName("a");
 
-var websiteLinks = document.getElementsByTagName("a");
-
-// Loops
-
-for (var i = 0; i < websiteLinks.length; i++) {
-  websiteLinks[i].href = "http://adf.ly/16956779/banner/" + websiteLinks[i].href;
+  for (var i = 0; i < websiteLinks.length; i++) {
+    websiteLinks[i].href = "http://adf.ly/16956779/banner/" + websiteLinks[i].href;
+  }
 }
